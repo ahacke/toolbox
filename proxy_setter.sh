5b0fc684d6f07700aca24cda8da7bdd818434c0b
André Hacke <andre.hacke1@volkswagen.de>
2021-05-07 12:33:11 +0200

    update proxy script to set upper and lowercase

--- proxy_setter.sh
+++ proxy_setter.sh
@@ -1,5 +1,7 @@
 echo "Loading proxy_setter..."
 
+# Sets the proxy configuration.
+# As applications are using lowercase or uppercase, both are set.
 # Provide the proxy variables
 # - by defining the variables here
 # - by loading another file with the variables via "source ./proxy-var-file.sh"
@@ -8,6 +10,10 @@ function set_proxies() {
 	export no_proxy=localhost,127.0.0.0/8,*.local
 	export http_proxy=http://$username:$password@$host:$port
 	export https_proxy=http://$username:$password@$host:$port
+	
+	export NO_PROXY=localhost,127.0.0.0/8,*.local
+	export HTTP_PROXY=http://$username:$password@$host:$port
+	export HTTPS_PROXY=http://$username:$password@$host:$port
 }
 
 function unset_proxies() {
