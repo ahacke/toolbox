086de3cc8c464eee6515b0f60ed626092df7cd68
André Hacke <andre.hacke1@volkswagen.de>
2021-07-21 16:41:25 +0200

    add osx wake up/lid open aliases

---
+++ osx-find-lid-open-time.sh
@@ -0,0 +1,6 @@
+
+alias get_osx_lidOpenUser='pmset -g log|grep -e "LidOpen/UserActivity"'
+
+alias get_osx_lidOpen='pmset -g log | grep LidOpen'
+
+alias get_osx_sleepWakeUp='pmset -g log|grep -e " Sleep  " -e " Wake  " -e " DarkWake  "'
