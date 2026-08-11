9f604d3aebf70447b38eaed7560eaafb0c3ca20a
André Hacke <ahacke@users.noreply.github.com>
2026-08-11 15:31:16 +0200

    add dev toolbox

---
+++ dev_toolbox.sh
@@ -0,0 +1,6 @@
+echo "Loading dev_toolbox..."
+
+# power user mode
+alias mkdocs="uv run mkdocs serve --livereload"
+alias mkdocs1="uv run mkdocs serve --livereload --dev-addr=localhost:8001"
+alias mkdocs2="uv run mkdocs serve --livereload --dev-addr=localhost:8002"
--- mac_toolbox.sh
+++ mac_toolbox.sh
@@ -11,6 +11,8 @@ alias mac_rpki_reset="sudo killall ctkd"
 alias mac_airid_reset="sudo killall ctkd; sudo pkill -9 com.apple.ifdreader"
 
 # Source: https://apple.stackexchange.com/questions/52064/how-to-find-out-the-start-time-of-last-sleep
-alias mac_osx_lidOpenUser='pmset -g log|grep -e "LidOpen/UserActivity"'
-alias mac_osx_lidOpen='pmset -g log | grep LidOpen'
 alias mac_osx_sleepWakeUp='pmset -g log|grep -e " Sleep  " -e " Wake  " -e " DarkWake  "'
+# Source: https://osxdaily.com/2011/07/14/get-exact-boot-sleep-and-wake-times-from-the-command-line/
+alias mac_osx_bootTime='sysctl -a |grep kern.boottime'
+alias mac_osx_sleepTime='sysctl -a |grep sleeptime'
+alias mac_osx_WakeTime='sysctl -a |grep waketime'
